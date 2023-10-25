709d9e3f30982fbbcfde6cbf0780c94098e7d2a7
SantiagoAHS <lionelsanti2018@gmail.com>
2023-10-25 09:29:59 -0600

    10 Eventos teclado JS

--- GraficCanvas/otro.js
+++ GraficCanvas/otro.js
@@ -7,34 +7,55 @@ let diry = 1;
 let idx = 10;
 let idy = 10;
 
-let lastX = idx;
-let lastY = idy;
-
 function bolita(x, y){
     // Generar valores RGB aleatorios
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
-    const color = `rgb(${red}, ${green}, ${blue}`;
+    const color = `rgb(${red}, ${green}, ${blue})`;
     
     ctx.fillStyle = color;
     ctx.beginPath();
     ctx.arc(x, y, 20, 0, Math.PI * 2);
     ctx.fill();
-    // Dibujar línea al punto anterior
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    lastX = x;
-    lastY = y;
 }
 
+function cuadrito(x, y){
+    ctx.fillStyle = hslColor(ini);
+    ini += 2;
+    ctx.fillRect(x, y, 40, 40);
+    ctx.strokeRect(x, y, 40, 40);
+}
+
+function emoji(x ,y){
+    ctx.font = '45px serif';
+    ctx.fillText('👻', x, y);
+}
+
+function emoji2(x ,y){
+    ctx.font = '45px serif';
+    ctx.fillText('🐔', x, y);
+}
+
+for(let i=0; i<5; i++){
+    const x =Math.round(Math.random()*30)
+    const y =Math.round(Math.random()*20)
+    emoji2(x * 20, y *20);
+}
+
+document.querySelector('body')
+    .addEventListener('keypress', (e) => {
+        e.preventDefault();
+    })
+
 setInterval(() => {
-    bolita(idx, idy);
-    if(dirx === 1 && diry === 1){
+    //ctx.clearRect(0, 0, 600, 400);
+    emoji(idx, idy);
+    if(idx > 650)idx=0;   
+    idx++; 
+    
+})
+    /*if(dirx === 1 && diry === 1){
         idx += 20;
         idy += 20;
     } else if (dirx === 1 && diry === 2){
@@ -51,6 +72,6 @@ setInterval(() => {
     if(idx > 590) dirx = 2;
     if(idx < 10) dirx = 1;
     if(idy > 390) diry = 2;
-    if(idy < 10) diry = 1;
-}, 45);
-
+    if(idy < 10) diry=1;
+}, 70);
+*/
